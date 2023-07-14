4b7089fabcec1d2e286a44e23f4619468fe83d6c
YungOleg <bilk5899@gmail.com>
2023-07-14 18:31:46 +0700

    getting started on the admin menu

--- handlers/admin/admin.py
+++ handlers/admin/admin.py
@@ -1,2 +1,8 @@
 from util import ADMIN_ID
+from load_bot import dp
+from aiogram import types
+from aiogram.dispatcher.filters import Command
 
+@dp.message_handler(user_id=ADMIN_ID, commands=Command("admin"))
+async def admin_menu(message: types.Message):
+    await message.answer("admin_menu")
